fcaf3dcfd4daa28b13f61eb16b21f2bcff043a15
Ying WANG <ying.wang@grafana.com>
2021-06-17 13:50:21 +0200

    change from reading fs.FS to use family object for generating ts

--- pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command.go
+++ pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command.go
@@ -2,62 +2,47 @@ package commands
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
 
-	"cuelang.org/go/cue"
-	"cuelang.org/go/cue/load"
 	"github.com/grafana/grafana/pkg/cmd/grafana-cli/utils"
+	"github.com/grafana/grafana/pkg/schema"
+	"github.com/grafana/grafana/pkg/schema/load"
 	"github.com/sdboyer/cuetsy/encoder"
 )
 
 func (cmd Command) generateDashboardTypeScripts(c utils.CommandLine) error {
 	dest := c.String("dest")
-	if err := generateTypeScriptFromFS(paths.BaseCueFS, dest); err != nil {
+	from := c.String("from")
+	if err := generateTypeScriptFromCUE(from, dest, paths, load.BaseDashboardFamily); err != nil {
 		return err
 	}
 
-	if err := generateTypeScriptFromFS(paths.DistPluginCueFS, dest); err != nil {
+	if err := generateTypeScriptFromCUE(from, dest, paths, load.DistDashboardFamily); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func generateTypeScriptFromFS(cueFS fs.FS, dest string) error {
-	err := fs.WalkDir(cueFS, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !d.IsDir() && filepath.Ext(path) == ".cue" {
-			if err := os.MkdirAll(dest, os.ModePerm); err != nil {
-				return err
-			}
-			fmt.Println("<<<<<<<<<<<<<<<<<1111111111", path)
-			b, err := generateTypeScriptFromFile(path)
-			if err != nil {
-				return err
-			}
-			fmt.Println("<<<<<<<<<<<<<<<<<222222222222")
-			if err := writeTypeScriptFiles(path, dest, b); err != nil {
-				return err
-			}
-		}
-		// fmt.Printf("path=%q, isDir=%v\n", path, d.IsDir())
-		return nil
-	})
-
+func generateTypeScriptFromCUE(from string, dest string, p load.BaseLoadPaths, loader func(p load.BaseLoadPaths) (schema.VersionedCueSchema, error)) error {
+	dash, err := loader(p)
 	if err != nil {
 		return err
 	}
 
+	inst, _ := dash.CUE().Reference()
+	b, err := encoder.Generate(inst, encoder.Config{})
+	if err != nil {
+		return err
+	}
+	writeTypeScriptFiles(from, dest, string(b))
+
 	return nil
 }
 
-func writeTypeScriptFiles(path string, dest string, content string) error {
-	fileName := filepath.Base(path)
+func writeTypeScriptFiles(from string, dest string, content string) error {
+	fileName := filepath.Base(from)
 	fd, err := os.Create(filepath.Join(dest, fileName[:len(fileName)-3]) + "ts")
 	if err != nil {
 		return err
@@ -65,17 +50,4 @@ func writeTypeScriptFiles(path string, dest string, content string) error {
 
 	fmt.Fprint(fd, content)
 	return nil
-}
-
-func generateTypeScriptFromFile(origin string) (string, error) {
-	loadedInstances := load.Instances([]string{origin}, nil)
-	instances := cue.Build(loadedInstances)
-	fmt.Println("<<<<<<<<<<<<<<<<<33")
-	b, err := encoder.Generate(instances[0], encoder.Config{})
-	fmt.Println("<<<<<<<<<<<<<<<<<44", b)
-	if err != nil {
-		return "", err
-	}
-
-	return string(b), nil
 }
--- pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command_test.go
+++ pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command_test.go
@@ -1,32 +1,23 @@
 package commands
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
-	"testing/fstest"
 
+	"github.com/grafana/grafana/pkg/schema/load"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCuetsyBasics(t *testing.T) {
-	t.Run("Testing generate typeScripts from cue schemas", func(t *testing.T) {
-		genCue, err := os.ReadFile("testdata/dashboard_cuetsy.cue")
-		require.NoError(t, err)
-
-		filesystem := fstest.MapFS{
-			"cue/ui/gen.cue": &fstest.MapFile{Data: genCue},
+	t.Run("Testing generate ts with cue schemas", func(t *testing.T) {
+		var baseLoadPaths = load.BaseLoadPaths{
+			BaseCueFS:       defaultBaseLoadPaths.BaseCueFS,
+			DistPluginCueFS: defaultBaseLoadPaths.DistPluginCueFS,
 		}
 
-		path, err := os.Getwd()
-		require.NoError(t, err)
+		err := generateTypeScriptFromCUE(baseLoadPaths, load.BaseDashboardFamily)
+		require.NoError(t, err, "error while loading base dashboard scuemata")
 
-		err = generateTypeScriptFromFS(filesystem, filepath.Join(path, "testdata"))
-
-		// err = validateScuemata(baseLoadPaths, load.BaseDashboardFamily)
-		require.NoError(t, err, "error when generate typeScript for base dashboard scuemata")
-
-		// err = validateScuemata(baseLoadPaths, load.DistDashboardFamily)
-		// assert.EqualError(t, err, "all schema should be valid with respect to basic CUE rules, Family.lineages.0.0: field #Panel not allowed")
+		err = generateTypeScriptFromCUE(baseLoadPaths, load.DistDashboardFamily)
+		require.NoError(t, err, "error while loading dist dashboard scuemata")
 	})
 }
