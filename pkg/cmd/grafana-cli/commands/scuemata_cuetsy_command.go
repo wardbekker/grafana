e3f7fd508dcc3bd194b1a5a92c4ae945c82f4696
Ying WANG <ying.wang@grafana.com>
2021-06-21 17:20:58 +0200

    generate panel ts

--- pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command.go
+++ pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command.go
@@ -4,47 +4,41 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 
 	"github.com/grafana/grafana/pkg/cmd/grafana-cli/utils"
-	"github.com/grafana/grafana/pkg/schema"
 	"github.com/grafana/grafana/pkg/schema/load"
 	"github.com/sdboyer/cuetsy/encoder"
 )
 
-var generatedFileName = 0
-
 func (cmd Command) generateDashboardTypeScripts(c utils.CommandLine) error {
 	dest := c.String("dest")
-	if err := generateTypeScriptFromCUE(dest, paths, load.BaseDashboardFamily); err != nil {
-		return err
-	}
 
-	if err := generateTypeScriptFromCUE(dest, paths, load.DistDashboardFamily); err != nil {
+	if err := generateTypeScriptFromCUE(dest, paths); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func generateTypeScriptFromCUE(dest string, p load.BaseLoadPaths, loader func(p load.BaseLoadPaths) (schema.VersionedCueSchema, error)) error {
-	dash, err := loader(p)
+func generateTypeScriptFromCUE(dest string, p load.BaseLoadPaths) error {
+	panelSchemaMap, err := load.ReadPanelModels(p)
 	if err != nil {
 		return err
 	}
 
-	inst, _ := dash.CUE().Reference()
-	b, err := encoder.Generate(inst, encoder.Config{})
-	if err != nil {
-		return err
+	for panelName, panelSchema := range panelSchemaMap {
+		inst, _ := panelSchema.CUE().Reference()
+		b, err := encoder.Generate(inst, encoder.Config{})
+		if err != nil {
+			return err
+		}
+		writeTypeScriptFiles(filepath.Join(dest, panelName+".ts"), string(b))
 	}
-	writeTypeScriptFiles(dest, string(b))
 	return nil
 }
 
 func writeTypeScriptFiles(dest string, content string) error {
-	fd, err := os.Create(filepath.Join(dest, "generatedFileName"+strconv.Itoa(generatedFileName)+"ts"))
-	generatedFileName++
+	fd, err := os.Create(dest)
 	if err != nil {
 		return err
 	}
